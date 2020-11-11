dae7b77f62fdc0a9a683912bbb7db56731d5ee2f
Gali Geller <gali1998@gmail.com>
2020-11-11 21:12:57 +0200

    set up infrstructure

---
+++ dist/server.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+app.listen(8000, function () {
+  console.log("server started on port 8000");
+});
