339806750dfe61fe2a8b37b4c4861733ea743935
Gali Geller <gali1998@gmai.com>
2020-12-31 18:56:33 +0200

    Added support for test environment

--- src/server.js
+++ src/server.js
@@ -11,7 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const basePath = "/home/gali/Desktop/db/";
+// example of linux directory:
+//const basePath = "/home/gali/Desktop/db/";
+
+var basePath;
+
+if (process.argv[process.argv.length - 1] == "prod"){
+    basePath = "C:/Users/gali1/OneDrive/Documents/Work/db/";
+} else{
+    basePath = "C:/Users/gali1/OneDrive/Documents/Work/test-db/";
+}
 
 app.post('/', (req, res) => {
 
